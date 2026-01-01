39c8c224c59f42bd13aa6f2f2c96a619ecae99cd
whattemup <fernandogarciarodriguez78@gmail.com>
2025-12-31 18:11:25 -0800

    Update config.py

--- backend/easyfinder/config.py
+++ backend/easyfinder/config.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "easyfinder")
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", "mock_key")
+FROM_EMAIL = os.getenv("FROM_EMAIL", "demo@easyfinder.ai")
+APP_ENV = os.getenv("APP_ENV", "local")
+MOCK_EMAIL_MODE = APP_ENV != "production"
